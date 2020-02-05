695ee62790802d0520bdebd21cdbe4b24ddc692c
Antonio Alvarado <ant.36alv@gmail.com>
2020-02-05 12:39:24 -0500

    Added all files I have recieved for the project.

---
+++ script.js
@@ -0,0 +1,43 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
+
+//!
+// (
+// )
+// ?
+// [
+// ]
+//
+// `
+// ~
+// ;
+// :
+// !
+// 
+// #
+// $
+// %
+// ^
+// &
+// *
+// +
+// =
+
+//https://www.ibm.com/support/knowledgecenter/SSV2LR/com.ibm.wbpm.imuc.doc/topics/rsec_characters.html
+
+// 0 1 2 3 4 5 6 7 8 9
+
+// A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+// a b c d e f g h i j k l m n o p q r s t u v w x y z
