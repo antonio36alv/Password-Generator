c6484960d145b1c37a26514c999242fc34574dbd
Antonio Alvarado <ant.36alv@gmail.com>
2020-02-07 10:16:40 -0500

    A lot of it is done. Need to make changes to the prompt div. Password now appears in the appropiate element, after being generated. Validation should be near ideal. Need to get rid of console.logs

--- script.js
+++ script.js
@@ -1,17 +1,95 @@
+console.log(document.querySelectorAll("form")[0].childNodes);
+var upperCheck = document.getElementById("upperCheck");
+var lowerCheck = document.getElementById("lowerCheck");
+var numCheck = document.getElementById("numCheck");
+var symbCheck = document.getElementById("symbCheck");  
+
+var listValues = document.getElementById("lengths");
+
+var lengthsList = document.getElementById("lengthsList");
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var doneBtn = document.querySelector("#doneBtn");
 
-var populate = document.getElementById("lengths");
-
 for(i = 8; i <= 128; i++){
   let newOP = document.createElement("OPTION");
   newOP.setAttribute("value", i);
-  populate.appendChild(newOP);
+  listValues.appendChild(newOP);
 }
 
-// Write password to the #password input
-function writePassword(criteria, charL) {
+// console.log(lengthsList.nextElementSibling.children[].value);
+console.log(lengthsList.nextElementSibling.childNodes);
+console.log(lengthsList.nextElementSibling.children[0]);
+
+doneBtn.addEventListener("click", function (){
+  //TODO comment
+  console.log("hhhh");
+  console.log(lengthsList.nextElementSibling.childNodes);
+  console.log(lengthsList.nextElementSibling.children[0]);
+  var criteriaIndexes = [];
+  if(!checkCriteria()) {
+    //triggers when criteria has no met the minimum  
+    console.log("something is wrong with your criteria");
+  } else {
+          //if uppper is checked push 0 to the array
+          if(upperCheck.checked === true){
+            criteriaIndexes.push(0);
+          }
+          //if lower is checked push 1 to the array
+          if(lowerCheck.checked === true){
+            criteriaIndexes.push(1);
+          }
+          ////if number is checked push 2 to the array
+          if(numCheck.checked === true){
+            criteriaIndexes.push(2);
+          }
+          //if symb is checked push 3 to the array
+          if(symbCheck.checked === true){
+            criteriaIndexes.push(3);
+          }
+  console.log(document.getElementById("lengthsList").value + " hello");
+  console.log(lengthsList.value + " hello");
+  //everything should have went well, now the criteria to write the password
+  writePassword(generatePassword(criteriaIndexes, lengthsList.value));
+  reset();
+  }
+});
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", function(){
+  //will reveal the div form responsible for prompting the user their criteria
+  // document.getElementById("criteriaDiv").style.zIndex = "-1";
+  console.log("show me the money");
+
+ document.getElementById("criteriaDiv").style.zIndex = "0";
+});
+
+function checkCriteria(){
+  if(!upperCheck.checked  && !lowerCheck.checked && !numCheck.checked && !symbCheck.checked){
+    //if everything is unchecked return false
+    console.log("failed bc no boxes were checked");
+    return false;
+  } else if(lengthsList.value==="") {
+    //if no value was entered into the list retrun false
+    console.log("failed bc of no length selected");
+    return false;
+  } else {
+    //only when both these tests are passed will we return true
+    console.log("check function passed");
+    console.log(listValues.childElementCount);
+    return true;
+  }
+}
+
+function writePassword(password) {
+  alert(password);
+
+  document.getElementById("password").textContent = password;
+}
+
+// generate password to the #password input
+function generatePassword(criteria, charL) {
   // var password = generatePassword();
   // var passwordText = document.querySelector("#password");
 
@@ -43,47 +121,12 @@ function writePassword(criteria, charL) {
     // char[Math.floor(Math.random() * charL)].charAt(Math.floor(Math.random() * ));
      passwordArr[i] = char[randomElement].charAt(Math.floor(Math.random() * char[randomElement].length));
     //passwordArr.push(char[randomElement].charAt(3));//Math.floor(Math.random() * char[randomElement].length));
-    alert(randomElement + " " + i);
+    // alert(randomElement + " " + i);
   }
-alert(passwordArr.join(""));
+return (passwordArr.join(""));
 //  https://www.sitepoint.com/javascript-fast-string-concatenation/
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", function(){
-
-  document.getElementById("criteriaDiv").style.zIndex = "2";
-  console.log("show me the money");
-});
-
-doneBtn.addEventListener("click", function(){
-  var upperCheck = document.getElementById("upperCheck");
-  var lowerCheck = document.getElementById("lowerCheck");
-  var numCheck = document.getElementById("numCheck");
-  var symbCheck = document.getElementById("symbCheck");  
-  var jawn = document.getElementById("jawn");
-  var criteriaIndexes = [];
-
-  if(!upperCheck.checked  && !lowerCheck.checked && !numCheck.checked && !symbCheck.checked){
-    //change alert, make user enter criteria again
-    alert("why you do this");
-  } else {
-          //if uppper is checked push 0 to the array
-          if(upperCheck.checked === true){
-            criteriaIndexes.push(0);
-          }
-          //if lower is checked push 1 to the array
-          if(lowerCheck.checked === true){
-            criteriaIndexes.push(1);
-          }
-          ////if number is checked push 2 to the array
-          if(numCheck.checked === true){
-            criteriaIndexes.push(2);
-          }
-          //if symb is checked push 3 to the array
-          if(symbCheck.checked === true){
-            criteriaIndexes.push(3);
-          }
-  }
-  writePassword(criteriaIndexes, jawn.value);//TODO need to appropriate char. length arguement.
-});
+function reset(){
+  document.getElementById("criteriaDiv").style.zIndex = "-1";
+}
